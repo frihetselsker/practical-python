e32df21bce7341a7f684cc242802c9143612a77f
keanomagnifico <roi.bachynskyi@stud.fils.upb.ro>
2024-07-18 21:06:02 +0300

    First session

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+iteration = 1
+while iteration < 11:
+	height *= 0.6
+	print(iteration, round(height, 4))
+	iteration += 1
+
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,36 @@
 # mortgage.py
 #
 # Exercise 1.7
+month = 0
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+amplifying_percent = 1 + rate/12
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+	month += 1
+	principal_with_new_percent = principal * amplifying_percent
+	principal = principal_with_new_percent - payment
+	if(principal < 0):
+		principal = 0
+		total_paid += principal_with_new_percent 
+	else:
+		total_paid += payment
+	if month >= extra_payment_start_month and month <= extra_payment_end_month:
+		principal_with_new_percent = principal
+		principal -= extra_payment
+		if(principal < 0):
+			principal = 0
+			total_paid += principal_with_new_percent
+		else:
+			total_paid += extra_payment
+	print(f'Month {month} - Paid ${total_paid:0.2f}, Left: ${principal:0.2f}')
+
+print(f'Total paid: ${total_paid:0.2f}')
+print(f'Months: {month}')
