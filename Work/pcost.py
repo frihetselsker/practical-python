dfa7e1406a447214744e70a3c4945225fa0c7b78
keanomagnifico <roi.bachynskyi@stud.fils.upb.ro>
2024-07-31 08:34:10 +0300

    Second section

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv, sys
+
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename) as file:
+        rows = csv.reader(file)
+        next(rows) # to remove the headers
+        for row in rows:
+            t = (row[0], int(row[1]), float(row[2]))
+            try:
+                total_cost += t[1] * t[2]
+            except ValueError:
+                print("Error: invalid file structure. Please, make changes to avoid this error")
+                exit()
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,50 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Returns the list of sets with made of a portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {
+                'name' : row[0],
+                'shares' : int(row[1]),
+                'price' : float(row[2])
+            }
+            portfolio.append(holding)
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except:
+                pass
+    return prices
+
+
+def make_report(portfolio, prices):
+    '''Returns the list of dictionaries with entries made of a portfolio file'''
+    report = []
+    for stock in portfolio:
+        stock['change'] = prices[stock['name']] - stock['price']
+        stock['price'] = prices[stock['name']]
+        report.append((stock['name'], stock['shares'], stock['price'], stock['change']))
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print('---------- ---------- ---------- -----------')
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {"$" + f"{price:0.2f}":>10s} {"$" + f"{change:0.2f}":>10s}')
+
+    return report
